40ecbd3fc2a2dbf3007f02301fd7638815013559
IndividualGames90 <IndividualGames@yandex.com>
2023-11-29 08:47:04 -0800

    code comments

--- Assets/Scripts/KirilacakObje.cs
+++ Assets/Scripts/KirilacakObje.cs
@@ -5,6 +5,7 @@ public class KirilacakObje : MonoBehaviour
     [SerializeField] GameObject _disableOnBroken;
     [SerializeField] GameObject _enableOnBroken;
 
+    /// <summary> Toggle breaking elements. </summary>
     public void Broken()
     {
         _disableOnBroken.SetActive(false);
